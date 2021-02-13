4c177d2734056d7d42c90c9a41573741dab5ae3a
icodesx <74471247+icodesx@users.noreply.github.com>
2021-02-12 23:35:35 -0300

    added booleans.js

---
+++ booleans.js
@@ -0,0 +1,7 @@
+// Booleans
+
+// False: false, 0, empty strings, NaN, null, undefined
+
+// All other values become true
+
+// Source: https://developer.mozilla.org/en-US/docs/Web/JavaScript/A_re-introduction_to_JavaScript
