e68a5e0e5f5e8bf18c74b30b7c413449508360c7
icodesx <74471247+icodesx@users.noreply.github.com>
2021-02-12 23:20:39 -0300

    First commit

---
+++ numbers.js
@@ -0,0 +1,10 @@
+// Numbers
+
+// Floor method rounds the number downwards to the nearest integer
+console.log(Math.floor(3/2));
+
+// Convert an string to an integer
+console.log(parseInt('015', 10));
+
+// Parse floating point numbers
+console.log(parseFloat('10'));
