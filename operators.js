46112a6c3b41bbadb48506d8515dd30b62b0640b
icodesx <74471247+icodesx@users.noreply.github.com>
2021-02-13 00:04:58 -0300

    added operators.js

---
+++ operators.js
@@ -0,0 +1,19 @@
+// Operators
+
+// Basic operatos in JavaScript are: +, -, *, /, %
+let a = 10 + 2;
+let b = 10 - 2;
+let c = 10 * 2;
+let d = 10 / 2;
+let e = 10 % 2;
+console.log(a, b, c, d, e);
+
+// String concatenation
+let word1 = 'Hello ';
+let word2 = 'World!';
+console.log(word1 + word2);
+
+// Comparisons
+console.log(5 > 1);
+console.log('5' == 5); // Double equal means that the operator makes type-coertion
+console.log('5' === 5); // Triple equal prevents this
